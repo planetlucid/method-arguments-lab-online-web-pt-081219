04d52e438f2b3b157d3de28c749d86ed54cd293c
Ryan S <planetlucid@gmail.com>
2019-08-12 21:38:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@ def introduction(name)
 end
 
 introduction(Josh)
+
+def introduction_with_language(name,)
+  puts "Hi, my name is #{name}and I am learning to program in the  #{language}."
+end
+
+introduction_with_language(Josh,Ruby)
