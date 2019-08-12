45a20353c585436543660aacaf850117e8b88898
Ryan S <planetlucid@gmail.com>
2019-08-12 21:40:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,9 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
+introduction(Josh)
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name}and I am learning to program in the  #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in the  #{language}."
 end
 
+introduction_with_language(Josh,Ruby)
